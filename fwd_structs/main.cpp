5759d63a0364b17d50a0c05013269384f162d4e9
Pudums <max_mail2002@bk.ru>
2021-05-07 13:41:20 +0300

    deeper recurse struct

--- fwd_structs/main.cpp
+++ fwd_structs/main.cpp
@@ -4,6 +4,9 @@
 int main() {
 	A *a = new A();
 	a->b = new B();
-	std::cout << a->x << "= a.x\n";
-	std::cout << a->b->x << "= a.b.x\n";
+	a->b->a = new A();
+	a->b->a->x = 2;
+	std::cout << a->x << " = a.x\n"; // 0
+	std::cout << a->b->x << " = a.b.x\n"; // 1
+	std::cout << a->b->a->x << " = a.b.a.x\n"; // 2
 }
